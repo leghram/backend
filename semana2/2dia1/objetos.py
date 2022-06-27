caab7aa0299bcfaa3251db52e33173cd02b0d01c
Miguel PL <mig.parlop@gmail.com>
2022-06-26 20:24:43 -0500

    encapsulamiento

--- semana2/2dia1/objetos.py
+++ semana2/2dia1/objetos.py
@@ -1,1 +1,35 @@
 print("OBJETOS")
+
+
+class Molde:
+    altura=2
+    color='azul'
+
+    def saludar(self):
+        print("hol es funcion")
+
+obj1 = Molde()
+
+print(obj1.color)
+obj1.saludar()
+
+
+class MoldeCompleto:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+    
+    def saluda(self):
+        print("hola ", self.nombre)
+
+
+obj2 = MoldeCompleto('MIGUEL',23)
+
+print(obj2.nombre)
+
+
+
+
+
+
+
