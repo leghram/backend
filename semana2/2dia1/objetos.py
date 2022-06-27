1e109b5585bfee69e68282da1cbaf4819af9f22b
Miguel PL <mig.parlop@gmail.com>
2022-06-26 20:39:38 -0500

    mas sobre datos privados

--- semana2/2dia1/objetos.py
+++ semana2/2dia1/objetos.py
@@ -28,8 +28,20 @@ obj2 = MoldeCompleto('MIGUEL',23)
 print(obj2.nombre)
 
 
+class ObjetoModelo:
+
+    def __init__(self, dato1, dato2):
+        self.variable1 = dato1
+        self.variable2 = dato2
+        #esto edefine que se trata de un permiso privado
+        self.__valorPrivado = 23
+
+    def Acceso(self):
+        print("Accede a privado por el canal correspondiente ", self.__valorPrivado)
 
 
 
 
-
+mas = ObjetoModelo(123,543)
+mas.Acceso()
+print(mas.__valorPrivado)
