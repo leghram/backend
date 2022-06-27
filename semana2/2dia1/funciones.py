154561eac842e8319935d599736bdd55257ef845
Miguel PL <mig.parlop@gmail.com>
2022-06-26 20:09:17 -0500

    objetos Inicio

--- semana2/2dia1/funciones.py
+++ semana2/2dia1/funciones.py
@@ -1,5 +1,36 @@
+from sqlite3 import enable_callback_tracebacks
+
+
 print("funciones")
 
 
 def Saludar():
     print("buenas tardes")
+
+
+def saludaNombre(nombre):
+    print (nombre)
+
+
+
+'''asdads esto tambien son comentarios'''
+
+
+def Suma(a, b):
+    valor = a + b
+    return valor
+
+
+def infi(a,*ena):
+    print(ena)
+
+
+infi(65,2543,23,34,43)
+
+
+
+
+
+
+
+
