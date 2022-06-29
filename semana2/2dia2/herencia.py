2b244c113848b90fe830fdf46c92c994a1691da3
Miguel PL <mig.parlop@gmail.com>
2022-06-28 19:14:14 -0500

    enviando herencia

--- semana2/2dia2/herencia.py
+++ semana2/2dia2/herencia.py
@@ -1,2 +1,29 @@
+
+
 print ("DIA 2 HERENCIA")
+
+class Usuario:
+    def __init__(self,nom, ape):
+        self.nombre = nom
+        self.apellido = ape
+
+class Alumno(Usuario):
+    def __init__(self, nom, ape, sec):
+        super().__init__(nom, ape)
+        self.seccion = sec
+
+    def info(self):
+        return {
+            'nombre':self.nombre,
+            'apellido':self.apellido,
+            'seccion':self.seccion 
+        }
+
+Alu1 = Alumno('Miguel', 'par', 4)
+Us1 = Usuario('oscar','asdf')
+
+
+veamos = Alu1.info();
+
+print(veamos)
 
